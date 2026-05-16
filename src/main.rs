e14dde479e88466681c7c5803a84e15343066ff1
James Baxter <jamesbaxter1989@gmail.com>
2026-05-16 22:18:02 +0100

    feat: window title, clear color, dual lighting, escape to quit

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,22 @@ use renderer::RendererPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "MassiveCityScale".into(),
+                resolution: (1280.0, 720.0).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(theme::themes::dark_neon().background))
         .add_plugins(WorldGenPlugin)
         .add_plugins(ThemePlugin)
         .add_plugins(CameraPlugin)
         .add_plugins(ScalePlugin)
         .add_plugins(RendererPlugin)
         .add_systems(Startup, spawn_light)
+        .add_systems(Update, quit_on_escape)
         .run();
 }
 
@@ -32,4 +41,21 @@ fn spawn_light(mut commands: Commands) {
         },
         Transform::from_xyz(1.0, 2.0, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
+    commands.spawn((
+        DirectionalLight {
+            illuminance: 3_000.0,
+            shadows_enabled: false,
+            ..default()
+        },
+        Transform::from_xyz(-1.0, 0.5, -1.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
+
+fn quit_on_escape(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut exit: EventWriter<AppExit>,
+) {
+    if keys.just_pressed(KeyCode::Escape) {
+        exit.send(AppExit::Success);
+    }
 }
